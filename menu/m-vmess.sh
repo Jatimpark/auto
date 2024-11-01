5b88fab7c507586bfb553364f4475f5071c9f92c
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-11-02 04:15:15 +0700

    Add files via upload

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -394,6 +394,124 @@ echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-vmess
 }
+
+function user-ws(){
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+                echo -e "\E[42;1;37m     Check Detail XRAY Vmess     \E[0m"
+                echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+                echo ""
+                echo "You have no existing clients!"
+                clear
+                exit 1
+        fi
+
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\E[42;1;37m     Check Detail XRAY Vmess     \E[0m"
+        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo " Select the existing client to view the config"
+        echo " Press CTRL+C to return"
+		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo "     No  User   Expired"
+        grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	    until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+        echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+user=$(cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+#tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
+#none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+domain=$(cat /etc/xray/domain)
+uuid=$(grep "},{" /etc/xray/config.json | cut -b 11-46 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+asu=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "bug.com",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF`
+ask=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "bug.com",
+      "port": "80",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "/vmess",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF`
+grpc=`cat<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "vmess-grpc",
+      "type": "none",
+      "host": "bug.com",
+      "tls": "tls"
+}
+EOF`
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmess_base643=$( base64 -w 0 <<< $vmess_json3)
+vmesslink1="vmess://$(echo $asu | base64 -w 0)"
+vmesslink2="vmess://$(echo $ask | base64 -w 0)"
+vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
+clear
+echo -e ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "\\E[0;41;36m        Vmess Account        \E[0m" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "Remarks        : ${user}" | tee -a /etc/log-create-vmess.log
+echo -e "Domain         : ${domain}" | tee -a /etc/log-create-vmess.log
+echo -e "Port TLS       : 443, 8443" | tee -a /etc/log-create-vmess.log
+echo -e "Port none TLS  : 80, 8880" | tee -a /etc/log-create-vmess.log
+echo -e "Port gRPC      : 443, 8443" | tee -a /etc/log-create-vmess.log
+echo -e "id             : ${uuid}" | tee -a /etc/log-create-vmess.log
+echo -e "alterId        : 0" | tee -a /etc/log-create-vmess.log
+echo -e "Security       : auto" | tee -a /etc/log-create-vmess.log
+echo -e "Network        : ws" | tee -a /etc/log-create-vmess.log
+echo -e "Path           : /vmess" | tee -a /etc/log-create-vmess.log
+echo -e "ServiceName    : vmess-grpc" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "Link TLS       : ${vmesslink1}" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "Link none TLS  : ${vmesslink2}" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "Link gRPC      : ${vmesslink3}" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "Expired On     : $exp" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo "" | tee -a /etc/log-create-vmess.log
+read -n 1 -s -r -p "Press any key to back on menu"
+
+m-vmess
+}
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;100;33m         • VMESS MENU •          \E[0m"
@@ -404,7 +522,8 @@ echo -e " [\e[36m•2\e[0m] Trial Account Vmess "
 echo -e " [\e[36m•3\e[0m] Extending Account Vmess "
 echo -e " [\e[36m•4\e[0m] Delete Account Vmess "
 echo -e " [\e[36m•5\e[0m] Check User Login Vmess "
-echo -e " [\e[36m•6\e[0m] User list created Account "
+echo -e " [\e[36m•6\e[0m] User List Log "
+echo -e " [\e[36m•7\e[0m] User List Akun Vmess "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -421,6 +540,7 @@ case $opt in
 4) clear ; del-ws ; exit ;;
 5) clear ; cek-ws ; exit ;;
 6) clear ; cat /etc/log-create-vmess.log ; exit ;;
+7) clear ; user-ws ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-vmess ;;
